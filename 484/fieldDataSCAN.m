806029966d1e1e30e8e3bdbcc58570042d6f42d5
Jwcaldwe <71105481+Jwcaldwe@users.noreply.github.com>
2020-11-09 00:32:27 -0500

    Update fieldDataSCAN.m

--- 484/fieldDataSCAN.m
+++ 484/fieldDataSCAN.m
@@ -4,8 +4,10 @@ clear all
 
 rng('shuffle')
 
-M = 520;
-N = 200;
+M = 600;
+N = 400;
+S = N/100;
+S = N/S;
 Rmax = 2;%maximum red weeds
 Bmax = 3;%maximum blue weeds
 Rdensity = 90;
@@ -23,7 +25,9 @@ for m = 1:M
         end
 end
 
+screen = zeros(M,N);
 mat = ones(M,N);
+matF = ones(M,N);
 
 %RED WEEDS
 for m = 1:M
@@ -43,35 +47,137 @@ for n = 50:100:N
         mat(m:m+10,n:n+10) = 2;
     end
 end
-
-
         
-cmap = zeros(5,3);
+cmap = zeros(6,3);
 %Color Selection:
 cmap(1,:) = [42 24 8]/100; %Brown
 cmap(2,:) = [0 100 0]/100; %Green
 cmap(3,:) = [100 0 0]/100; %Red
 cmap(4,:) = [0 0 100]/100; %Blue
 cmap(5,:) = [100 100 100]/100; %White
+cmap(6,:) = [0 0 0]/100; %Black
 
-mat2 = repelem(mat,2,2);
+for m = 1:M
+    for n = 1:N
+        if (mat(m,n) < screen(m,n))
+            matF(m,n) = screen(m,n);
+        else
+            matF(m,n) = mat(m,n);
+        end
+    end
+end
+
+mat2 = repelem(matF,2,2);
 imshow(mat2, cmap)
 
-i = 1;
-k = 1;
+j = 1;
 
 %Scanning for the Weeds and Detecting as White Dots.
-while(i <= M)
-    while(k <= N)
-        if (mat(i,k) == 3 || mat(i,k) == 4)
-            mat(i:i+4,k:k+4) = 5;
-            mat2 = repelem(mat,2,2);
+for k = 1:S:N-1
+    if(j)
+        for i = 1:S:M-1
+            screen(i:i+S,k) = 6;
+            screen(i:i+S,k+S) = 6;
+            screen(i,k:k+S) = 6;
+            screen(i+S,k:k+S) = 6;
+            for v = k:k+S-1
+                for u = i:i+S-1
+                    if(mat(u,v) == 3 || mat(u,v) == 4)
+                        mat(u:u+4,v:v+4) = 1;
+                        for m = 1:M
+                            for n = 1:N
+                                if (mat(m,n) < screen(m,n))
+                                    matF(m,n) = screen(m,n);
+                                else
+                                    matF(m,n) = mat(m,n);
+                                end
+                            end
+                        end
+                        mat2 = repelem(matF,2,2);
+                        imshow(mat2, cmap)
+                    end
+                end
+            end
+            for m = 1:M
+                for n = 1:N
+                    if (mat(m,n) < screen(m,n))
+                        matF(m,n) = screen(m,n);
+                    else
+                        matF(m,n) = mat(m,n);
+                    end
+                end
+            end
+            mat2 = repelem(matF,2,2);
             imshow(mat2, cmap)
+            screen(i:i+S,k) = 1;
+            screen(i:i+S,k+S) = 1;
+            screen(i,k:k+S) = 1;
+            screen(i+S,k:k+S) = 1;
+%             if(matF(i,k) == 3 || matF(i,k) == 4)
+%                 matF(i:i+4,k:k+4) = 5;
+%                 mat2 = repelem(matF,2,2);
+%                 imshow(mat2, cmap)
+%             end
         end
-        k = k + 1;
+        j = 0;
+    else
+        for i = M-S:-S:0
+            if i == 0
+                i = 1;
+            end
+            screen(i:i+S,k) = 6;
+            screen(i:i+S,k+S) = 6;
+            screen(i,k:k+S) = 6;
+            screen(i+S,k:k+S) = 6;
+            for v = k:k+S-1
+                for u = i:i+S-1
+                    if(mat(u,v) == 3 || mat(u,v) == 4)
+                        mat(u:u+4,v:v+4) = 1;
+                        for m = 1:M
+                            for n = 1:N
+                                if (mat(m,n) < screen(m,n))
+                                    matF(m,n) = screen(m,n);
+                                else
+                                    matF(m,n) = mat(m,n);
+                                end
+                            end
+                        end
+                        mat2 = repelem(matF,2,2);
+                        imshow(mat2, cmap)
+                    end
+                end
+            end
+            for m = 1:M
+                for n = 1:N
+                    if (mat(m,n) < screen(m,n))
+                        matF(m,n) = screen(m,n);
+                    else
+                        matF(m,n) = mat(m,n);
+                    end
+                end
+            end
+            mat2 = repelem(matF,2,2);
+            imshow(mat2, cmap)
+            screen(i:i+S,k) = 1;
+            screen(i:i+S,k+S) = 1;
+            screen(i,k:k+S) = 1;
+            screen(i+S,k:k+S) = 1;
+%             if(matF(i,k) == 3 || matF(i,k) == 4)
+%                 matF(i:i+4,k:k+4) = 5;
+%                 mat2 = repelem(matF,2,2);
+%                 imshow(mat2, cmap)
+%             end
+        end
+        j = 1;
     end
-    i = i + 1;
-    k = 1;
+%     while(k <= N)
+%         if (matF(i,k) == 3 || matF(i,k) == 4)
+%             matF(i:i+4,k:k+4) = 5;
+%             mat2 = repelem(matF,2,2);
+%             imshow(mat2, cmap)
+%         end
+%         k = k + 1;
+%     end
 end
 
 
@@ -112,6 +218,3 @@ end
 %         end
 %     end
 % end
-
-
-
