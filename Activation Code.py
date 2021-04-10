7aa524db558571eb64481b63e2bdec81bb071a60
camoody1210 <71102766+camoody1210@users.noreply.github.com>
2021-04-10 10:43:48 -0400

    Update Activation Code.py

--- Activation Code.py
+++ Activation Code.py
@@ -3,22 +3,24 @@ from time import sleep                  #load time controls in the form of sleep
 
 inl = 23
 in2 = 24                                #enable 3 input pins necessary for L298N operation. in1 and in2 determine polarity of motor. en determines power delivered
-ena = 25
+en = 25
 
 GPIO.setmode (GPIO.BCM)
 GPIO.setup (in1, GPIO.OUT)
 GPIO.setup (in2, GPIO.OUT)              #setup L298N pins corresponding to necessary Raspberri Pi pins
-GPIO.setup (ena, GPIO.OUT)
+GPIO.setup (en, GPIO.OUT)
 GPIO.output (in1, GPIO.LOW)             #set first input to low side
 GPIO.output (in2, GPIO.HIGH)            #set second input to high side
-laser = GPIO.PWM(ena, 100)              #setup up pulse width modulation control for laser out of 100
+laser = GPIO.PWM(en, 1000)              #setup up pulse width modulation control for laser out of 1000
 
-laser.start(0)                          #start duty cycle at 0 to ensure that laser begins off
+laser.start(100)                        #start duty cycle at 0 to ensure that laser begins off
+x = 1
 
-def activation(pwr, t):
-    x = 1
-    while (x == 1):                     #begin on/off cycle loop
-        laser.ChangeDutyCycle(pwr)      #turn laser on at full power by changing duty cycle to 100, should supply 3.3 v
-        sleep(t)                        #halt code for 2 seconds meaning laser will remain on
-        laser.ChangeDutyCycle(0)        #turn off laser by changing duty cycle back to zero
-    return
+while (x == 1):                         #begin on/off cycle loop
+        GPIO.output (in1, GPIO.HIGH)           
+        GPIO.output (in2, GPIO.LOW)     
+        sleep(2)                        #halt code for 2 seconds meaning laser will remain on
+        GPIO.output (in1, GPIO.LOW)           
+        GPIO.output (in2, GPIO.LOW)  
+        x=x+1
+        
