79fa34b569a438a3cfd1abd1aae57538d5e6765a
camoody1210 <71102766+camoody1210@users.noreply.github.com>
2021-04-11 18:48:07 -0400

    Update Activation Code.py

--- Activation Code.py
+++ Activation Code.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 from time import sleep                  #load time controls in the form of sleep command
 
 in1 = 23
-in2 = 24                                #enable 3 input pins necessary for L298N operation. in1 and in2 determine polarity of motor. en determines power delivered
+in2 = 24                                #enable 3 input pins necessary for L298N operation. in1 and in2 determine polarity of motor, en determines power delivered
 en = 25
 
 GPIO.setmode (GPIO.BCM)
@@ -10,17 +10,17 @@ GPIO.setup (in1, GPIO.OUT)
 GPIO.setup (in2, GPIO.OUT)              #setup L298N pins corresponding to necessary Raspberri Pi pins
 GPIO.setup (en, GPIO.OUT)
 GPIO.output (in1, GPIO.LOW)             #set first input to low side
-GPIO.output (in2, GPIO.LOW)            #set second input to high side
-laser = GPIO.PWM(en, 100)              #setup up pulse width modulation control for laser out of 100
+GPIO.output (in2, GPIO.LOW)             #set second input to high side
+laser = GPIO.PWM(en, 100)               #setup up pulse width modulation control for laser at 100 Hz
 
-laser.start(100)                          #start duty cycle at 0 to ensure that laser begins off
+laser.start(0)                          #start duty cycle at 0 to ensure that laser begins off
 x = 1
-while x == 1:                     #begin on/off cycle loop
-    GPIO.output(in1,GPIO.HIGH)
+while x == 1:                           #begin on/off cycle loop
+    GPIO.output(in1,GPIO.HIGH)          #laser turnes on
     GPIO.output(in2,GPIO.LOW)
-    sleep(2)                        #halt code for 2 seconds meaning laser will remain on
+    sleep(2)                            #halt code for 2 seconds meaning laser will remain on
     GPIO.output(in1,GPIO.LOW)
-    GPIO.output(in2,GPIO.LOW)
-    x = x+1
+    GPIO.output(in2,GPIO.LOW)           #laser turns off
+    x = x+1                             #end while loop
         
         
